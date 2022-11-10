c3f12e0372f6d3d04a3bfd01a933fdfb3a777e39
RobMRowe <>
2022-11-10 12:51:49 +0000

    fleshed out logic, added basic styling

--- src/Takeaway.js
+++ src/Takeaway.js
@@ -21,22 +21,21 @@ class Takeaway {
   printMenu() {
     for (let i = 0; i < takeaway.menu.length; i++) {
       let menuItemContainer = document.createElement("div");
-      menuItemContainer.setAttribute('class', "menu-item");
-      menuItemContainer.setAttribute('style', "width: 240px; height: 200px; border: thin black solid; background: white; display: flex; flex-direction: column; align-items: center; justify-content: space-around;");
+      menuItemContainer.classList.add("menu-item");
       let menuItemHeading = document.createElement("h2");
       menuItemHeading.innerHTML = takeaway.menu[i].name;
-      menuItemHeading.setAttribute('style', "text-align: center; max-width: 220px; height: 70px;");
-      menuItemContainer.appendChild(menuItemHeading);
       let menuItemDetailsContainer = document.createElement("div");
-      menuItemDetailsContainer.setAttribute('class', "menu-item-details");
-      menuItemDetailsContainer.setAttribute('style', "width: 220px; height: 80px; display: flex; justify-content: space-between; align-items: center; max-width: 220px;");
+      menuItemDetailsContainer.classList.add("menu-item-details-container");
       let menuItemDetails = document.createElement("p");
+      menuItemDetails.classList.add("menu-item-details");
       menuItemDetails.innerHTML = takeaway.menu[i].details;
-      menuItemDetailsContainer.appendChild(menuItemDetails);
-      menuItemContainer.appendChild(menuItemDetailsContainer);
       let menuItemPrice = document.createElement("p");
+      menuItemPrice.classList.add("menu-item-price");
       menuItemPrice.innerHTML = Intl.NumberFormat('en-GB', { style: 'currency', currency: 'GBP' }).format(takeaway.menu[i].price);
+      menuItemContainer.appendChild(menuItemHeading);
+      menuItemDetailsContainer.appendChild(menuItemDetails);
       menuItemDetailsContainer.appendChild(menuItemPrice);
+      menuItemContainer.appendChild(menuItemDetailsContainer);
       menuContainer.appendChild(menuItemContainer);
     }
   }
